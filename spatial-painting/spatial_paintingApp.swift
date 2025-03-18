b7cb04b02fc1362be40fa4566ec227243ba24477
kanakanho <kahoshibakichi@gmail.com>
2025-03-18 09:44:21 +0900

    Initial Commit

---
+++ spatial-painting/spatial_paintingApp.swift
@@ -0,0 +1,33 @@
+//
+//  spatial_paintingApp.swift
+//  spatial-painting
+//
+//  Created by blueken on 2025/03/18.
+//
+
+import SwiftUI
+
+@main
+struct spatial_paintingApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
